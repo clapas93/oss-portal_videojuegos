d98a04ec8fef05af87c36645aae6774ef026e7d2
Noatino <paranoid.tony@gmail.com>
2015-04-16 13:17:55 -0500

    after we discuss the new way to make push into the master branch

---
+++ Videojuegos/src/User/UpdateStudent.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+
+
+/**
+ *
+ * @author antoniogalvan
+ */
+public class UpdateStudent extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        String view ="";
+        String footer = "footer.jsp";
+        
+        switch(path){
+            case "/updatestudent":
+                view = "profileUpdate.jsp";
+                request.setAttribute("view", view); 
+                request.setAttribute("title", "Manage Games");
+                request.setAttribute("footer", footer); 
+                request.getRequestDispatcher("layout.jsp").forward(request, response);
+            break;
+            case "/studentsave":
+                view = "registro.jsp";
+                request.setAttribute("view", view); 
+                request.setAttribute("title", "Manage Games");
+                request.setAttribute("footer", footer); 
+                request.getRequestDispatcher("layout.jsp").forward(request, response);
+                if (ServletFileUpload.isMultipartContent(request)) {
+
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload= new ServletFileUpload(factory);
+            upload.setSizeMax(Long.MAX_VALUE);
+
+            List listUploadFiles = null;
+            FileItem item = null;
+        try (PrintWriter out = response.getWriter()) {
+            try {
+                listUploadFiles = upload.parseRequest((RequestContext) request);
+
+                Iterator it = listUploadFiles.iterator();
+                while (it.hasNext()) {
+                    item = (FileItem) it.next();
+                    if (!item.isFormField()) {
+                        if (item.getSize() > 0) {
+                            String nombre = item.getName();
+                            String tipo = item.getContentType();
+
+                            long tamanio = item.getSize();
+
+                            String extension = nombre.substring(nombre.lastIndexOf("."));
+
+                            out.println("Nombre: " + nombre + "<br>");
+                            out.println("Tipo: " + tipo + "<br>");
+                            out.println("Extension: " + extension + "<br>");
+                        }
+                    }
+                }
+
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }catch(Exception e) {
+                e.printStackTrace();
+        }
+        }
+            break;
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
