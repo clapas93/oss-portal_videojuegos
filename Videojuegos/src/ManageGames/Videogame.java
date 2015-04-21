bed4f36636c48a28ee923fae20a64cc1e31f427a
clapas93 <clapas93@ciencias.unam.mx>
2015-04-21 17:03:12 -0500

    Merge master with ManageGames solved

--- Videojuegos/src/ManageGames/Videogame.java
+++ Videojuegos/src/ManageGames/Videogame.java
@@ -50,7 +50,7 @@ public class Videogame {
         this.adminEmail="admin@oss.com";
     }
     
-<<<<<<< HEAD
+
     /**
     * Constructs a new Videogame with attributes that the system doesn't generate.
     * @param front          Videogame front.
@@ -61,7 +61,7 @@ public class Videogame {
     * @param title          Videogame title.
     * @param description    Videogame description.
     * @param videoUrl       video trailer.
-    * @param adminEmail     Admin's email who uploaded the videogame
+    * @param adminemail     Admin's email who uploaded the videogame
     */
     public Videogame(String front,String classification,float price,String storageRoute,String genre,String title,String description,String videoUrl,String adminemail){
         this.id=0;
