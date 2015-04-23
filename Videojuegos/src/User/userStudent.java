bfa387ac69297980e42ce838739ad0f646b3f387
magdy-zon <ganabbisativa@hotmail.com>
2015-04-23 03:05:20 -0500

    first instance profileUpdate solving1

--- Videojuegos/src/User/userStudent.java
+++ Videojuegos/src/User/userStudent.java
@@ -90,4 +90,28 @@ public class userStudent extends user {
     public void setHistory(String history) {
         this.history = history;
     }   
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getLastname1(){
+        return lastname1;
+    }
+    public void setLastname1(String lastname1){
+        this.lastname1 = lastname1;
+    }
+    public String getLastname2(){
+        return lastname2;
+    }
+    public void setLastname2(String lastname2){
+        this.lastname2 = lastname2;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
 }
