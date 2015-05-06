5c8f0f6c7a47cee7f92643ca49db81e4ac4bd7b1
ellemiranda <eduardomiranda@ciencias.unam.mx>
2015-05-05 21:30:25 -0500

    ConnectionDB created Loan model updated

--- Videojuegos/src/ConnectionDB/ConnectionDB.java
+++ Videojuegos/src/ConnectionDB/ConnectionDB.java
@@ -27,8 +27,8 @@ public class ConnectionDB {
     try {
       
       this.host = "localhost";
-      this.port = "5433";
-      //this.port = "5432";
+      //this.port = "5433";
+      this.port = "5432";
       this.database = "videoGames";
       this.user = "Optimal";
       this.password = "OptimalSOfSo!";
