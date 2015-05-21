8175ffe910dfd636c2caebcfd84bbc2a60606112
Noatino <paranoid.tony@gmail.com>
2015-05-21 15:09:13 -0500

    Controller to get the number of downloads for a given id_game is now done

--- Videojuegos/src/TransactionsController/Transactions.java
+++ Videojuegos/src/TransactionsController/Transactions.java
@@ -5,6 +5,12 @@
  */
 package TransactionsController;
 
+import ConnectionDB.ConnectionDB;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *This class will provide at the transactions a 
  * controller in agreement with the model, view, controller 
@@ -14,7 +20,24 @@ public class Transactions {
     
     /**
      * With this method we can update the number of downloads 
-     * for a videogame.
+     * for a videogame. Return -1 if don't exist this videogame
+     * @param videogame_id
+     * @return int
      */
     
+    
+    public int numberOfDownloads(int videogame_id){
+        ConnectionDB data = new ConnectionDB();
+        String query = "SELECT COUNT(*) FROM download WHERE idgame ="+ String.valueOf(videogame_id)+";";
+        ResultSet res = data.select(query);
+        int resultado = -1;
+        try {
+            if(res.next()){
+                resultado = Integer.parseInt(res.getObject(1).toString());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Transactions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
 }
