c17697df0e258445b97ed8daae33c357a7273b72
Noatino <paranoid.tony@gmail.com>
2015-05-21 00:15:29 -0500

    before I do pull of the master

---
+++ Videojuegos/src/TransactionsController/Transactions.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TransactionsController;
+
+/**
+ *This class will provide at the transactions a 
+ * controller in agreement with the model, view, controller 
+ * @author antoniogalvan
+ */
+public class Transactions {
+    
+    /**
+     * With this method we can update the number of downloads 
+     * for a videogame.
+     */
+    
+}
