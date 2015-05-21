c6f7664177296121ad2335620306a73ab07332eb
Magdiel Juárez Guerrero <ganabbisativa@hotmail.com>
2015-05-21 13:21:57 -0500

    conflicts in database

--- Videojuegos/src/ConnectionDB/connectiondb.java
+++ Videojuegos/src/ConnectionDB/connectiondb.java
@@ -11,54 +11,114 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author magdiel
- */
-public class connectiondb {
-    
-  public Connection connectionDB(){
-
-        /**
-         * host: servidor en el cuaĺ está montado PostgreSQL
-         * port: puerto de escucha de PostgreSQL
-         * database: nombre de la base de datos a la cuál realizaremos conexión
-         * user: Usuario con el cuál podemos acceder a la base de datos
-         * password: Contraseñá del user para acceder a la base de datos.
-         * stringConection: Cadena en la que se concatenan todos los parámetros para una correcta conexión
-         * a la base de datos.
-         */
+public class ConnectionDB {
+  
+  private Connection connection = null;
+  private ResultSet rs= null;
+  private Statement stat = null;
+  private String host;
+  private String port;
+  private String database;
+  private String user;
+  private String password;
+  private String stringConection;
+  
+  public ConnectionDB(){
+    try {
       
-        String host;
-        String port;
-        String database;
-        String user;
-        String password;
-        String stringConection;
-        
-        /**
-         * Se intenta hacer una conexión a la base de datos y se cacha cualquier tipo de error
-         */
-        
-        try {
-            
-            host = "localhost";
-
-            //port = "5433";
-            port = "5432";
-            database = "videoGames";
-            user = "Optimal";
-            password = "OptimalSOfSo!";
-            stringConection = "jdbc:postgresql://" + host + ":" + port + "/" + database;
-            Class.forName("org.postgresql.Driver");
-            System.out.println("OK ... CONECTO CON DRIVER");
-            
-            return DriverManager.getConnection(stringConection, user, password);
-            
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Problemas de Conexión: " + e.toString());
-            return null;
-        }
+      this.host = "localhost";
+      //this.port = "5433";
+      this.port = "5432";
+      this.database = "videoGames";
+      this.user = "Optimal";
+      this.password = "OptimalSOfSo!";
+      this.stringConection = "jdbc:postgresql://" + host + ":" + port + "/" + database;
+      Class.forName("org.postgresql.Driver");
+      System.out.println("OK ... CONECTO CON DRIVER");
+      
+      this.connection = DriverManager.getConnection(stringConection, user, password);
+      
+      
+    } catch (ClassNotFoundException | SQLException e) {
+      System.out.println("Problemas de Conexión: " + e.toString());
     }
   }
 
+  /**
+   * This method makes a select query to the database
+   * @param select
+   * @return ResultSet result of the query
+   */
+  public ResultSet select(String select){    
+    try{
+      this.stat = this.connection.createStatement();
+      ResultSet resultset = this.stat.executeQuery(select);
+      System.out.println(select);
+      if(resultset != null){
+        System.out.println("OK ... bien1.1");
+      }else{
+        System.out.println("NO EXISTE INFORMACION");
+      }      
+      return resultset;
+    }catch(Exception e){
+      System.out.println("Error...."+ e.toString());
+      return null;
+    }
+  } 
+
+
+  /**
+  * Check the connection with db and make an insert consult
+  * @param SQL
+  * @return Boolean returns true if the query was succesfull
+  */
+  public boolean insert(String SQL){    
+    try{
+      stat = this.connection.createStatement();
+      stat.executeUpdate(SQL);
+      System.out.println(SQL);
+      return  true;
+    }catch(Exception e){
+      System.out.println("Error...."+ e.toString());
+      return false;
+    }
+  } 
+
+  /**
+  * Check the connection with db and make an update action
+  * @param SQL
+  * @return Boolean returns true if the query was succesfull
+  */
+  public boolean update(String SQL){    
+    try{
+      this.stat = this.connection.createStatement();
+      stat.executeUpdate(SQL);
+      System.out.println(SQL);
+      return true;
+    }catch(Exception e){
+      System.out.println("Error...."+ e.toString());
+      return false;
+    }
+  } 
+
+
+  /**
+  * Check the connection with db and make an delete action
+  * @param SQL
+  * @return Boolean returns true if the query was succesfull
+  */
+  public boolean delete(String SQL){    
+    try{
+      this.stat = this.connection.createStatement();
+      stat.executeUpdate(SQL);
+      System.out.println(SQL);
+      
+      return true;
+    }catch(Exception e){
+      System.out.println("Error...."+ e.toString());
+      return false;
+    }
+  } 
+
+
+}
