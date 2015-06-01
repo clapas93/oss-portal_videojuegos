048cff3ec8c6b1885b9406a187fcabfda459f127
ellemiranda <eduardomiranda@ciencias.unam.mx>
2015-06-01 01:08:22 -0500

    Transactions controller

--- Videojuegos/src/TransactionsController/TransactionsController.java
+++ Videojuegos/src/TransactionsController/TransactionsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TransactionsController;
 
 import java.io.IOException;
@@ -13,42 +8,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.json.simple.JSONObject;
-import TransactionsController.Transactions;
 
 /**
- *
- * @author lalo
+ * Optimal Software Solutions
+ * Project : Pulse Games
+ * This is the Controller to do the transaction for the system.
+ * @author Miranda Sáncehz Luis Eduardo
  */
 public class TransactionsController extends HttpServlet {
 
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
     
-  }
+    }
 
-  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-  /**
-   * Handles the HTTP <code>GET</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+	processRequest(request, response);
+    }
 
   /**
    * Handles the HTTP <code>POST</code> method.
@@ -74,9 +70,15 @@ public class TransactionsController extends HttpServlet {
         JSONObject obj = new JSONObject();
         idGame = (String)request.getParameter("idGame");
         System.out.println("idGame   "+idGame);
+        if(email==null||email.equals("")){
+          email = "null@mail.com";
+        }
         success = transaction.registerDownload(email, idGame);
+        String name  = transaction.getGameName(idGame);
+        double credit = transaction.getCredits(email);
         if(success){
-          obj.put("success",true);
+          obj.put("name",name);
+          obj.put("credit",credit);
         }else{
           obj.put("error",false);
         }
@@ -85,15 +87,14 @@ public class TransactionsController extends HttpServlet {
       break;
     }
   }
-
-  /**
-   * Returns a short description of the servlet.
-   *
-   * @return a String containing servlet description
-   */
-  @Override
-  public String getServletInfo() {
-    return "Short description";
-  }// </editor-fold>
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+	return "Short description";
+    }// </editor-fold>
 
 }
